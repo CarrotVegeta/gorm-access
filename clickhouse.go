cd52370e2ae2ef2ff1a7ae2eabb56d0dfdbb3d24
Carrotvegeta <chuan.li@bangcle.com>
2024-07-03 15:59:21 +0800

    feat clickhouse增加方法

--- clickhouse.go
+++ clickhouse.go
@@ -1,6 +1,9 @@
 package dac
 
-import "fmt"
+import (
+	"fmt"
+	"gorm.io/gorm"
+)
 
 func init() {
 	RegisterDatabase(Clickhouse, &ClickHouseDatabase{})
@@ -14,14 +17,44 @@ type ClickhouseOperator struct {
 	OperatorI
 }
 
+// Limit 实现Limit方法
+func (m *ClickHouseDatabase) Limit(db *gorm.DB, page, pageSize int64) *gorm.DB {
+	db = db.Limit(int(pageSize)).Offset(int(page * pageSize))
+	return db
+}
+
 func (co *ClickhouseOperator) BuildQuery(condition Condition, qf *QueryFilter) {
 	switch condition.Operator {
 	case Equal:
 		co.Equals(condition, qf)
 	case NotEqual:
 		co.NotEqual(condition, qf)
-
+	case GreaterThanOrEqual:
+		co.GreaterThanOrEqual(condition, qf)
+	case GreaterThan:
+		co.GreaterThan(condition, qf)
+	case LessThanOrEqual:
+		co.LessThanOrEqual(condition, qf)
+	case LessThan:
+		co.LessThan(condition, qf)
+	case In:
+		co.In(condition, qf)
 	}
+}
+func (co *ClickhouseOperator) In(condition Condition, qf *QueryFilter) {
+	qf.And(condition.Key+" in (?)", condition.Value)
+}
+func (co *ClickhouseOperator) GreaterThanOrEqual(condition Condition, qf *QueryFilter) {
+	qf.And(condition.Key+" >= ?", condition.Value)
+}
+func (co *ClickhouseOperator) GreaterThan(condition Condition, qf *QueryFilter) {
+	qf.And(condition.Key+" > ?", condition.Value)
+}
+func (co *ClickhouseOperator) LessThanOrEqual(condition Condition, qf *QueryFilter) {
+	qf.And(condition.Key+" <= ?", condition.Value)
+}
+func (co *ClickhouseOperator) LessThan(condition Condition, qf *QueryFilter) {
+	qf.And(condition.Key+" < ?", condition.Value)
 }
 func (co *ClickhouseOperator) NotBlank(condition Condition, qf *QueryFilter) {
 	qf.And(fmt.Sprintf("%s <> ''  ", condition.Field), condition.Value)
